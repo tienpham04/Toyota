1616edb078c171a7ef07e4c315133f034b537d94
HoangQuyCoder <162110303+HoangQuyCoder@users.noreply.github.com>
2024-05-16 09:08:06 +0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -6,8 +6,5 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
-class SignUpForm(UserCreationForm):
-    class Meta:
-        model=User
-        fields = ['username','email','password1'] 
+
 
