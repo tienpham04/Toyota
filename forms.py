223ff8581d7c30eb4b17b4009a848cc3c0a91497
HoangQuyCoder <162110303+HoangQuyCoder@users.noreply.github.com>
2024-05-16 08:52:42 +0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,1 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=65)
+    password = forms.CharField(max_length=65, widget=forms.PasswordInput)
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields = ['username','email','password1'] 
 
